46039aa85bb13d9cf4e7390d7896effb7570c871
ingrid-munoz <77245013+ingrid-munoz@users.noreply.github.com>
2021-01-30 22:39:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "Ejercicio 5. Visual Studio Code";
 echo "Ejercicio 5. Visual Studio Code. Añadiendo otra linea";
 echo "Ejercicio 5. Visual Studio Code. Añadiendo una linea mas";
+echo "Ejercicio 5. Visual Studio Code. Modificando desde github";
 ?>
